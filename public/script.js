a377e82614a70fb23cc5bc07a0ccd27a81e04d03
Jathur <jathursikan@gmail.com>
2021-01-02 18:32:03 -0500

    .

--- public/script.js
+++ public/script.js
@@ -1,13 +1,9 @@
 const chatForm = document.getElementById('chat-form');
 const chatMessages = document.querySelector('.chat-messages');
 
-const socket = io();
 
-// Join chatroom
-socket.emit('joinRoom', { username, room });
 
 // Message from server
-socket.on('message', message => {
-    console.log(message);
+socket.on("message", () => {
+    console.log('Welcome to Hyper!'); // ojIckSD2jqNzOqIrAGzL
 });
-
