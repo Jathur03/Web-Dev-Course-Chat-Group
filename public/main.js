90f42177242d05f9f1fbc3cc00fed1585e341ed8
Jathur <jathursikan@gmail.com>
2021-01-03 13:56:20 -0500

    .

--- public/main.js
+++ public/main.js
@@ -7,5 +7,17 @@ chatForm.addEventListener('submit', (e) => {
 
     let msg = e.target.elements.msg.value;
 
-    console.log(msg);
-})
+    socket.emit('chatMessage', msg);
+});
+
+socket.on('message', message => {
+    console.log(message);
+    outputMessage(message);
+});
+
+function outputMessage(message) {
+    const div = document.createElement('div');
+    div.classList.add('message');
+    div.innerHTML = `<p class="text-warning">${message}</p>`
+    document.querySelector('.chat-messages').appendChild(div);
+};
--- server.js
+++ server.js
@@ -12,7 +12,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Run when client connects
 io.on('connection', socket => {
-    
+    socket.on('chatMessage', (msg) => {
+        io.emit('message', msg);
+    });
 });
 
 const PORT = 4000 || process.env.PORT;
