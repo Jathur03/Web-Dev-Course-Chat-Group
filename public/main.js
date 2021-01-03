3005eaf82a86fa20f3bdcfdc5ea9aee454f512ee
Jathur <jathursikan@gmail.com>
2021-01-03 12:13:55 -0500

    .

--- public/main.js
+++ public/main.js
@@ -1,23 +1,18 @@
-const chatMessages = document.querySelector('.chat-messages');
 const chatForm = document.getElementById('chat-form');
+const chatMessages = document.querySelector('.chat-messages');
 
 const socket = io();
 
+// Message from server
 socket.on('message', message => {
     console.log(message);
     outputMessage(message);
+
+    // Scroll down
+    chatMessages.scrollTop = chatMessages.scrollHeight;
 });
 
-function outputMessage(message) {
-    const div = document.createElement('div');
-    div.classList.add('message');
-    div.innerHTML = `<p class="meta">Hello <span>5:89</span></p>
-    <p class="text">
-        ${message.text}
-    </p>`;
-    document.querySelector('.chat-messages').appendChild(div);
-};
-
+// Message submit
 chatForm.addEventListener('submit', e => {
     e.preventDefault();
 
@@ -31,3 +26,14 @@ chatForm.addEventListener('submit', e => {
     e.target.elements.msg.value = '';
     e.target.elements.msg.focus()
 });
+
+// Output message to DOM
+function outputMessage(message) {
+    const div = document.createElement('div');
+    div.classList.add('message');
+    div.innerHTML = `<p class="meta">Jathur <span>6:75</span></p>
+    <p class="text">
+        ${message.text}
+    </p>`;
+    document.querySelector('.chat-messages').appendChild(div);
+};
