10f650278e8f8c5ec7f5f48356260b82193de8a8
Jathur <jathursikan@gmail.com>
2021-01-03 13:43:00 -0500

    .

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
-const formatMessage = require('./utils/messages');
 
 const app = express();
 const server = http.createServer(app);
@@ -13,18 +12,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Run when client connects
 io.on('connection', socket => {
-    socket.emit('message', 'Welcome to Hyper');
-
-    // Listen for chatMessage
-    socket.on('chatMessage', (msg) => {
-        io.emit('message', msg);
-    });
-
-    socket.broadcast.emit('message', formatMessage('User', 'A user has joined the chat'));
-
-    socket.on('disconnect', () => {
-        io.emit('message', formatMessage('A user has left the chat'));
-    });
+    
 });
 
 const PORT = 4000 || process.env.PORT;
